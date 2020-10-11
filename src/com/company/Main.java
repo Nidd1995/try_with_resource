45d99202eaeff1fe6c4e7bee49672e3d990657e1
nidhi <nidhi.verma@kuliza.com>
2020-10-11 15:11:44 +0530

    Example on java try with resource

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(new File("/home/nidhi/MyProjects/test.txt"));
+
+            while (sc.hasNext()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+
+            if (sc != null) {
+                sc.close();
+            }
+        }
+
+    }
+}
